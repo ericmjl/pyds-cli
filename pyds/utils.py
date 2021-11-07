dae2af5e33ff24829d7c4c5a48613280fb79c1ed
Eric Ma <ericmajinglong@gmail.com>
2021-11-07 08:23:24 -0500

    Fix utils.py

--- pyds/utils.py
+++ pyds/utils.py
@@ -1,3 +1,4 @@
+"""Utility functions for pyds."""
 from pathlib import Path
 from jinja2 import Template
 import yaml
@@ -5,22 +6,29 @@ from loguru import logger
 import subprocess
 import os
 from pyprojroot import here
-from rich import print
 
 
 CONDA_EXE = os.getenv("CONDA_EXE")
 ANACONDA = os.getenv("anaconda")
-# CONDA_EXE = "conda"
 
 
 def read_template(path: Path) -> Template:
-    """Return the jinja2 template."""
+    """Return the jinja2 template.
+
+    :param path: Path to template.
+    :returns: A jinja2 Template object.
+    """
     with open(path, "r+") as f:
         return Template(f.read())
 
 
 def write_file(template_file: Path, information: dict, destination_file: Path):
-    """Write a template file to disk."""
+    """Write a template file to disk.
+
+    :param template_file: Path to a template.
+    :param information: Dictionary of information to populate in the template.
+    :param destination_file: Path to where the filled template should be placed.
+    """
     template = read_template(template_file)
     text = template.render(**information)
     destination_file.touch()
@@ -29,7 +37,13 @@ def write_file(template_file: Path, information: dict, destination_file: Path):
 
 
 def read_config():
-    """Read configuration file."""
+    """Read PyDS configuration file.
+
+    The PyDS configuration file is always located in the home directory
+    and has the name `.pyds.yaml`.
+
+    :returns: A dictionary of PyDS configurations.
+    """
     config_path = Path.home() / ".pyds.yaml"
     with config_path.open("r+") as f:
         return yaml.safe_load(f.read())
@@ -43,12 +57,20 @@ def run(
     log: bool = True,
     show_out: bool = False,
 ):
-    """Convenience function to run a shell command while also logging the command."""
+    """Convenience function to run a shell command while also logging the command.
+
+    :param cmd: The command to run.
+    :param cwd: The working directory in which to run the code.
+    :param shell: Passed to `subprocess.run`'s `shell` argument.
+    :param capture_output: Passed to `subprocess.run`'s `capture_output` argument.
+    :param log: Whether or not to log the command to screen.
+    :param show_out: Whether or not to show the output of `cmd` to the terminal.
+    :returns: The result of `subprocess.run`.
+    """
     if log:
         logger.info(f"+ {cmd}")
 
     if show_out:
-        print("Running command while showing output!")
         out = subprocess.run(
             cmd,
             cwd=cwd,
@@ -71,6 +93,11 @@ def get_conda_env_name(env_file="environment.yml", cwd: Path = Path(".")):
     This function can be executed from anywhere _within_ a project directory.
 
     We search for `environment.yml` by default.
+
+    :param env_file: Name of the environment file.
+    :param cwd: Where to begin recursive search of the project root directory.
+    :returns: The environment name.
+    :raises FileNotFoundError: when the `environment.yml` file cannot be found.
     """
     try:
         with open(here(cwd) / env_file, "r+") as f:
@@ -84,7 +111,15 @@ def get_conda_env_name(env_file="environment.yml", cwd: Path = Path(".")):
 
 
 def get_env_bin_dir(env_file="environment.yml", cwd: Path = Path(".")):
-    """Get the environment bin directory."""
+    """Get the environment bin directory.
+
+    Used to identify the absolute path to an environment's executables.
+
+    :param env_file: Name of the environment file
+        from which we look for the environment name.
+    :param cwd: Where to begin recursive search of the project root directory.
+    :returns: Path to the conda environment's `bin` directory.
+    """
     env_name = get_conda_env_name(env_file=env_file, cwd=cwd)
     ENV_BIN_DIR = f"{ANACONDA}/envs/{env_name}/bin"
     return ENV_BIN_DIR
