af5b866ccf168497ce69e39686a8a1268fd72917
Eric Ma <ericmjl@users.noreply.github.com>
2021-11-19 04:22:03 +0000

    Got all tests working for project initialization.

--- tests/cli/test_project.py
+++ tests/cli/test_project.py
@@ -1,32 +1,27 @@
 """Tests for project creation and management."""
-import os
 from pathlib import Path
 
-from pyds.cli import app
+from caseconverter.caseconverter import snakecase
+
 from pyds.cli.project import TEMPLATE_DIR
 from typer.testing import CliRunner
 
 runner = CliRunner()
 
 
-def test_project_initialize(tmp_path, project_name: str = "asdf"):
+def test_project_initialize(initialized_project):
     """Test for project initialization.
 
-    :param tmp_path: Built-in pytest fixture to generate temporary directory.
-    :param project_name: Name of the test project.
+    :param initialized_project: conftest.py fixture.
     """
-    os.chdir(tmp_path)
-    runner.invoke(
-        app, ["project", "initialize"], input=f"{project_name}\nasdf\nMIT\nY\nY\nY\n"
-    )
-
-    project_dir = tmp_path / "asdf"
+    tmp_path, project_name = initialized_project
+    project_dir = tmp_path / project_name
 
     expected_files = list(TEMPLATE_DIR.glob("*/**/*.j2"))
     for f in expected_files:
         f = f.resolve().relative_to(TEMPLATE_DIR).with_suffix("")
         if "src" in f.parts:
             f_str = str(f)
-            f_str = f_str.replace("src", "asdf")
+            f_str = f_str.replace("src", snakecase(project_name))
             f = Path(f_str)
         assert (project_dir / f).exists()
