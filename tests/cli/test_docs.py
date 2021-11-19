b120c94f0b5c8cfc4e669069195dc133659e1545
Eric Ma <ericmjl@users.noreply.github.com>
2021-11-19 07:04:07 +0000

    Add test for build command.

---
+++ tests/cli/test_docs.py
@@ -0,0 +1,20 @@
+"""Tests for docs."""
+from typer.testing import CliRunner
+from pyds.cli import app
+import os
+
+
+runner = CliRunner()
+
+
+def test_build(initialized_project):
+    """Execution test for docs build command.
+
+    :param initialized_project: conftest.py fixture for our initialized project.
+    """
+    tmp_path, project_name = initialized_project
+    project_dir = tmp_path / project_name
+    os.chdir(project_dir.resolve())
+
+    result = runner.invoke(app, ["docs", "build"])
+    assert result.exit_code == 0
