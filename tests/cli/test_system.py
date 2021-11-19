973dca7914f6054fd4161bbcca703c4e76984444
Eric Ma <ericmjl@users.noreply.github.com>
2021-11-19 07:13:53 +0000

    Add tests for system commands.

---
+++ tests/cli/test_system.py
@@ -0,0 +1,12 @@
+"""Tests for system commands."""
+from typer.testing import CliRunner
+from pyds.cli import app
+
+
+runner = CliRunner()
+
+
+def test_status():
+    """Execution test for docs build command."""
+    result = runner.invoke(app, ["system", "status"])
+    assert result.exit_code == 0
