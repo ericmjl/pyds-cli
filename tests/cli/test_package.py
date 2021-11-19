70dc8453a49f2002727ad626a124fd1e3eca057b
Eric Ma <ericmjl@users.noreply.github.com>
2021-11-19 13:02:53 +0000

    Start tests for pyds package command.

---
+++ tests/cli/test_package.py
@@ -0,0 +1,16 @@
+"""Tests for the pyds package subcommand."""
+
+
+from typer.testing import CliRunner
+from pyds.cli import app
+
+runner = CliRunner()
+
+
+def test_reinstall(initialized_project):
+    """Test reinstall sub-command.
+
+    :param initialized_project: conftest.py fixture for our initialized project.
+    """
+    result = runner.invoke(app, ["package", "reinstall"])
+    assert result.exit_code == 0
