01cabf47aa48691e6ce2d20aa176e617703bd909
Eric Ma <ericmjl@users.noreply.github.com>
2021-12-24 20:44:04 +0000

    Git add and commit after bumpversion to ensure line changes are committed.

--- pyds/cli/package.py
+++ pyds/cli/package.py
@@ -55,6 +55,7 @@ def publish(
         response = typer.confirm("Please double-check: is the version bump done right?")
         if response:
             run(f"bumpversion {bump} --verbose", show_out=True, activate_env=True)
+            run("git add . && git commit")
             run("rm dist/*")
             run(f"python -m build {here()}", activate_env=True)
             run(f"twine upload -r {to} dist/*", activate_env=True)
