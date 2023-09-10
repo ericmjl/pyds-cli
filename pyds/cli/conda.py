c7831497d843e99372fbc64f6b3b5884c4b324b3
Eric Ma <ericmajinglong@gmail.com>
2023-09-10 19:25:02 -0400

    feat(conda): add ensure_base_env function
    - Added a new function `ensure_base_env` to ensure that the user is in the base conda environment before running certain commands.
    - This function checks the value of the `CONDA_DEFAULT_ENV` environment variable and raises an `EnvironmentError` if the user is not in the base environment.
    - This function is used in the `rebuild` and `update` commands to ensure that the user is in the correct environment before proceeding.
    
    [END COMMIT MESSAGE]

--- pyds/cli/conda.py
+++ pyds/cli/conda.py
@@ -1,10 +1,24 @@
 """Conda surrogate commands."""
 import yaml
+import os
 from typer import Typer
 
 from ..utils import CONDA_EXE, run
 
 app = Typer()
+
+
+def ensure_base_env():
+    """Ensure that the user is in the base conda environment.
+
+    :raises EnvironmentError: If the user is not in the base conda environment.
+    """
+    conda_env = os.environ.get("CONDA_DEFAULT_ENV")
+
+    if conda_env != "base":
+        raise EnvironmentError(
+            "Please run `conda activate base` before running `pyds conda rebuild`!"
+        )
 
 
 @app.command()
@@ -16,20 +30,19 @@ def clean():
 @app.command()
 def rebuild():
     """Rebuild the conda environment from scratch."""
+    ensure_base_env()
     with open("environment.yml", "r+") as f:
         env_config = yaml.safe_load(f)
         project_name = env_config["name"]
-    run(f"bash -c 'conda env remove -n {project_name}'", show_out=True)
-    run(
-        f"bash -c 'source activate base && {CONDA_EXE} env update -f environment.yml'",
-        show_out=True,
-    )
+    run(f"conda env remove -n {project_name}", show_out=True)
+    run(f"{CONDA_EXE} env update -f environment.yml", show_out=True)
     run("python -m pip install -e .", show_out=True, activate_env=True)
 
 
 @app.command()
 def update():
     """Update the conda environment associated with the project."""
+    ensure_base_env()
     run(f"bash -c 'source activate base && {CONDA_EXE} env update -f environment.yml'")
 
 
