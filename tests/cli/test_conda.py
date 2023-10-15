23ad0d064bcee6a0cbaa6d8086ed20803a693afb
Eric Ma <ericmjl@users.noreply.github.com>
2023-10-15 11:12:50 -0400

    test(cli): change pytest fixture scope from session to function (#14)
    * test(cli): change pytest fixture scope from session to function
    
    This commit modifies the scope of the pytest fixture in the cli tests from "session" to "function".
    This change ensures that the fixture is set up and torn down for each individual test function,
    providing a more isolated testing environment.
    
    * chore: Update pre-commit hook versions and replace darglint with pydoclint
    
    This commit updates the versions of pre-commit hooks to their latest versions.
    It also replaces the darglint hook with pydoclint for better documentation linting.
    Additionally, a minor change is made to the release notes documentation
    to ensure a newline at the end of the file.
    
    * test(conftest): change pytest fixture scope from function to session
    
    This commit modifies the scope of the pytest fixture in the conftest.py file.
    The scope has been changed from "function" to "session",
    which means the fixture will only be invoked once per session,
    instead of once per test function.
    This change could potentially improve the performance of the test suite
    by reducing the number of times the fixture is set up and torn down.
    
    * test(conda): update exit code expectations in conda tests
    
    The conda tests for rebuild and update now expect an exit code of 1 instead of 0.
    This change reflects the assumption that these tests are always executed
    from within a conda environment, not the base environment.

--- tests/cli/test_conda.py
+++ tests/cli/test_conda.py
@@ -17,20 +17,31 @@ def test_clean():
 def test_rebuild(initialized_project):
     """Execution test for conda rebuild wrapper.
 
+    Because we assume that the test is always executed from within a conda env,
+    it must return exit code 1, rather than 0.
+    This is b/c we expect the the `pyds conda update` command to be executed
+    from within the _base_ env and not a project-specific env.
+
+
     :param initialized_project: conftest.py fixture for our initialized project.
     """
     tmp_path, project_name = initialized_project
     os.chdir(tmp_path / project_name)
     result = runner.invoke(app, ["conda", "rebuild"])
-    assert result.exit_code == 0
+    assert result.exit_code == 1
 
 
 def test_update(initialized_project):
     """Execution test for conda update wrapper.
+
+    Because we assume that the test is always executed from within a conda env,
+    it must return exit code 1, rather than 0.
+    This is b/c we expect the the `pyds conda update` command to be executed
+    from within the _base_ env and not a project-specific env.
 
     :param initialized_project: conftest.py fixture for our initialized project.
     """
     tmp_path, project_name = initialized_project
     os.chdir(tmp_path / project_name)
     result = runner.invoke(app, ["conda", "update"])
-    assert result.exit_code == 0
+    assert result.exit_code == 1
