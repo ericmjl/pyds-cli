503193cc245100279d980f033296401f3196cc6d
Eric Ma <ericmajinglong@gmail.com>
2024-03-31 09:49:22 -0400

    test(cli): enhance project initialization and update tests
    This commit enhances the testing suite for the project initialization and update functionalities in the CLI. The changes include:
    - Renaming `test_project_initialize` to `test_project_init` for consistency with the command it tests.
    - Adding assertions to check the exit code of the `project init` and `project update` commands, ensuring they execute successfully.

--- tests/cli/test_project.py
+++ tests/cli/test_project.py
@@ -2,12 +2,24 @@
 
 from typer.testing import CliRunner
 
+from pyds.cli import app
+
 runner = CliRunner()
 
 
-def test_project_initialize(initialized_project):
+def test_project_init(initialized_project):
     """Test for project initialization.
 
     :param initialized_project: conftest.py fixture for our initialized project.
     """
-    pass
+    result = runner.invoke(app, ["project", "init"])
+    assert result.exit_code == 0
+
+
+def test_project_update(initialized_project):
+    """Test for project initialization.
+
+    :param initialized_project: conftest.py fixture for our initialized project.
+    """
+    result = runner.invoke(app, ["project", "update"])
+    assert result.exit_code == 0
