f56e7b25e17ae44ec6d9186288a8be6e24f49993
Eric Ma <ericmajinglong@gmail.com>
2024-06-01 10:20:54 -0400

    feat(tests): add test for .env file presence in project directory
    - Introduce a new test case in `test_project.py` to verify the presence of a `.env` file in the initialized project directory.
    - Import the `os` module to enable directory changes and file existence checks within the test.
    
    This enhancement ensures that projects initialized with our CLI tool are correctly setting up environment variables, improving configuration management and consistency across development environments.

--- tests/cli/test_project.py
+++ tests/cli/test_project.py
@@ -1,10 +1,23 @@
 """Tests for project creation and management."""
 
+import os
+
 from typer.testing import CliRunner
 
 from pyds.cli import app
 
 runner = CliRunner()
+
+
+def test_dotenv_presence(initialized_project):
+    """Assert that the.env file is present in the initialized project directory.
+
+    :param initialized_project: conftest.py fixture for our initialized project.
+    """
+    tmp_path, project_name = initialized_project
+    os.chdir(tmp_path / project_name)
+
+    assert (tmp_path / project_name / ".env").exists()
 
 
 def test_project_update(initialized_project):
