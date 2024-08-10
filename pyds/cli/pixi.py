29cbc5c940510374fa4782ed06ea2bf3d76eae48
Eric Ma <ericmajinglong@gmail.com>
2024-08-10 19:00:52 -0400

    fix(pixi)📝: Improve error message for missing .pixi directory
    - Provide more informative error message when .pixi directory is missing
    - Suggest running `pixi install` to create environment

--- pyds/cli/pixi.py
+++ pyds/cli/pixi.py
@@ -12,13 +12,20 @@ app = typer.Typer()
 
 @app.command()
 def rebuild():
-    """Rebuild the pixi environment."""
+    """Rebuild the pixi environment.
+
+    This command should be used
+    whenever you suspect that your Pixi environment might be broken.
+    This may show up in packages cryptically having files not being found.
+    """
     # Check that the directory `.pixi/` exists.
     if not Path(".pixi").exists():
-        error_msg = """No `.pixi/` directory found!
-You should either navigate to the root of your project and run `pyds pixi rebuild`,
-or else directly run `pixi install` in your terminal.
-"""
+        error_msg = (
+            "No `.pixi/` directory found, "
+            "so it looks like there's nothing to rebuild! "
+            "Feel free to run `pixi install --manifest-path pyproject.toml` "
+            "to create your environment."
+        )
         raise FileNotFoundError(error_msg)
     rm("-rf", ".pixi")
     pixi("install", "--manifest-path", "pyproject.toml")
